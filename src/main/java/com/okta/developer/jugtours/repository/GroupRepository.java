8d3be72feeaca281ff7d3c119a7b5d4457bcdc42
SG0222580 <mariusz.stelmaszczuk@sabre.com>
2020-10-24 23:06:43 +0200

    moving

--- src/main/java/com/okta/developer/jugtours/repository/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.okta.developer.jugtours.repository;
-
-import com.okta.developer.jugtours.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    Group findByName(String name);
-}
